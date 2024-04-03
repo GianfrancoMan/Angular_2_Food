7d7543c7cf21e492a49d2e4cbe3227dd09f75a90
Gianfranco <gfman@inwind.it>
2024-04-03 17:05:35 +0200

    added cors options

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit, inject } from '@angular/core';
 import { Fruit } from '../models/fruit.model';
 import { Observable } from 'rxjs';
@@ -15,11 +15,15 @@ export class DataService {
 
   currentFruits:Fruit[] = [];
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*'}),
+  };
+
 
   constructor() { }
 
   public getData():Observable<Fruit[]> {
-    return this.http.get<Fruit[]>(`${this.URL_API}`);
+    return this.http.get<Fruit[]>(`${this.URL_API}`, this.httpOptions);
   }
 
   public getImage(fruitName:string):string {
