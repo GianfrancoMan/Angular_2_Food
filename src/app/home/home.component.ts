6af551fc876b0d44c777575e3a6e525d09521a67
Gianfranco <gfman@inwind.it>
2024-04-04 16:32:21 +0200

    add log

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit{
           this.dataService.setCurrentFruits(this.fruitData);
         },
         error: (err) => {
-          console.log(`Error: ${err}`);
+          console.log(`Error: ${err.message}`);
         },
         complete: ()=> {
           this.route.queryParams.subscribe( (p:any) => {
